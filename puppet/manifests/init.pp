2b4303a5efb777fae796a0d62dae6ed6094dd17d
Brett Gullan <Brett.Gullan@abc.net.au>
2016-02-05 08:20:11 +1100

    Expanded path list

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,4 +1,7 @@
-Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+# default path
+Exec {
+  path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
+}
 
 
 class system-update {
